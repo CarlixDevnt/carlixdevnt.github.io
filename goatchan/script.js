d26eb7a20fc656d4dcfea5b689318cc9bfdc5a42
Carlos Pérez <88063767+CarlixDevnt@users.noreply.github.com>
2023-08-11 00:49:10 +0200

    Actualizar script.js

--- goatchan/script.js
+++ goatchan/script.js
@@ -10,3 +10,26 @@ themeToggle.addEventListener("click", () => {
         themeToggle.innerHTML = '<i class="fas fa-sun"></i>';
     }
 });
+
+// ... (previous code)
+
+// Get the saved theme preference (if available) or default to light theme
+const savedTheme = localStorage.getItem('theme');
+if (savedTheme === 'dark') {
+    body.classList.add('dark-theme');
+    document.getElementById('theme-icon').classList.add('fa-moon');
+} else {
+    document.getElementById('theme-icon').classList.add('fa-sun');
+}
+
+themeToggle.addEventListener('click', () => {
+    body.classList.toggle('dark-theme');
+    
+    const themeIcon = document.getElementById('theme-icon');
+    themeIcon.classList.toggle('fa-sun', !body.classList.contains('dark-theme'));
+    themeIcon.classList.toggle('fa-moon', body.classList.contains('dark-theme'));
+    
+    // Save the user's preference to local storage
+    const newTheme = body.classList.contains('dark-theme') ? 'dark' : 'light';
+    localStorage.setItem('theme', newTheme);
+});
